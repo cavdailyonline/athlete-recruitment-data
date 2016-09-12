42ce97fe45b6a72439787c03f5b3ca43d05ac711
Maxwell Luo <mustmax123@yahoo.com>
2016-09-12 15:39:25 -0400

    document script

--- athletes.py
+++ athletes.py
@@ -19,15 +19,20 @@ all_sports_links = set()
 all_sports_names = set()
 all_sports_roster = {}
 
+# Get link for each sport
+# there are only 19 sports because track and cross country athletes are all listed on one page
 for sport in sports:
     link = sport.get("href")
     all_sports_links.add(link)
 
 count = 1
 for sport_link in all_sports_links:
+    # read url to specific sport
     x = urllib.request.urlopen(all_sports_base + sport_link).read()
+
     sportname = sport_link.split("/")[2]
     print("SPORT NAME: %s" % sportname)
+
     soup_sport = BeautifulSoup(x, "html.parser")
     roster = []
     for name_element in soup_sport.find_all("tr", class_="player-row"):
@@ -35,21 +40,28 @@ for sport_link in all_sports_links:
         if "http://onlyfans.cstv.com" not in name_element.a.get("href"):
             player = {}
             player["name"] = name_element.a.get_text()
+
+            # get url to player info (includes name, hometown, high school and other stats -- varies by sport)
             player_info_link = name_element.a.get("href")
             y = urllib.request.urlopen(all_sports_base + player_info_link).read()
             soup = BeautifulSoup(y, "html.parser")
+
+            # check if hometown and high school are provided (hometown and high school are always the second and third elements
             infoExists = len(soup.find_all("font", color="#FFFFFF"))
+
             player["hometown"] = soup.find_all("font", color="#FFFFFF")[1].text.split(":")[
                 1] if infoExists >= 2 else "Not provided"
             player["highschool"] = soup.find_all("font", color="#FFFFFF")[2].text.split(":")[
                 1] if infoExists >= 3 else "Not provided"
             roster.append(player)
+
             print("Added data for %s" % player["name"])
     print(roster)
     print("Sport %d/%d complete" % (count, len(all_sports_links)))
     count += 1
     all_sports_roster[sportname] = roster
 
+# write to csv
 with open(csvout, 'w') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=',', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
     csvwriter.writerow(["Sport", "Name", "Hometown", "High School"])
